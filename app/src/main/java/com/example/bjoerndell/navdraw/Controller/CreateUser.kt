8b151fcabc60a8f46798d66ba43713d53bb3f2c0
Björn Viehmann <B.Viehmann@gmx.de>
2018-06-23 13:49:36 +0200

    Registrierung angepasst

--- app/src/main/java/com/example/bjoerndell/navdraw/Controller/CreateUser.kt
+++ app/src/main/java/com/example/bjoerndell/navdraw/Controller/CreateUser.kt
@@ -1,54 +1,77 @@
 package com.example.bjoerndell.navdraw.Controller
 
+import android.app.ProgressDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.TextView
 import android.widget.Toast
 import com.example.bjoerndell.navdraw.Model.User
 import com.example.bjoerndell.navdraw.R
+import com.example.bjoerndell.navdraw.Utilitiy.EXTRA_PASSWORDLENGTH
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_create_user.*
-import org.w3c.dom.Text
+
+
 
 class CreateUser : AppCompatActivity() {
+
+    lateinit var txtEmail: TextView
+    lateinit var txtPin: TextView
+    lateinit var fbAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_user)
 
         createUserRegisterBtn.setOnClickListener(){
-            val firstName = findViewById<TextView>(R.id.txtFirstNameReg)
-            val lastName = findViewById<TextView>(R.id.txtLastNameReg)
+            val firstName = findViewById<TextView>(R.id.txtEmail)
+            val lastName = findViewById<TextView>(R.id.txtPin)
 
-            if (firstName.text.toString().isEmpty()) {
-                firstName.error = "Bitte Vornamen eingeben"
-            } else if (lastName.text.toString().isEmpty()) {
-                lastName.error = "Bitte Nachnamen eingeben"
+            txtEmail = findViewById(R.id.txtEmail)
+            txtPin = findViewById(R.id.txtPin)
+            fbAuth = FirebaseAuth.getInstance()
+
+            registerUser()
+        }
+
+    }
+
+
+    fun registerUser (){
+
+        val email = txtEmail.text.toString().trim()
+        val pin = txtPin.text.toString().trim()
+
+
+        if(TextUtils.isEmpty(email)){
+            txtEmail.error = "Gib deine E-Mail-Adresse ein"
+            return
+        }
+        if (TextUtils.isEmpty(pin)){
+            txtPin.error = "Gib eine PIN ein"
+            return
+        } else if (TextUtils.getTrimmedLength(pin) < EXTRA_PASSWORDLENGTH){
+            txtPin.error = "Die PIN muss aus mind. $EXTRA_PASSWORDLENGTH Zahlen bestehen"
+            return
+        }
+
+        val progress = ProgressDialog(this)
+        progress.setMessage("Du wirst registriert")
+        progress.setCancelable(false)
+        progress.show()
+
+        fbAuth.createUserWithEmailAndPassword(email, pin).addOnCompleteListener {task: Task<AuthResult> ->
+            progress.dismiss()
+            if(task.isSuccessful){
+                Toast.makeText(this,"Du bist registriert", Toast.LENGTH_SHORT).show()
             } else {
-                createNewUser(firstName.text.toString(), lastName.text.toString())
-
-                //Textfelder zurücksetzen
-                firstName.text = null
-                lastName.text = null
+                val exception = task.exception
+                Toast.makeText(this,exception.toString(), Toast.LENGTH_LONG).show()
             }
         }
-
     }
-
-    fun createNewUser(firstName: String, lastName: String){
-
-        val ref = FirebaseDatabase.getInstance().getReference("User")
-
-        val userId = ref.push().key
-        val user = User(userId, firstName, lastName, "ohne")
-
-        ref.child(userId).setValue(user).addOnCompleteListener(){
-            Toast.makeText(applicationContext, "Benutzer wurde angelegt!", Toast.LENGTH_SHORT).show()
-
-        }
-
-
-
-    }
-
 }
--- app/src/main/java/com/example/bjoerndell/navdraw/Utilitiy/Constants.kt
+++ app/src/main/java/com/example/bjoerndell/navdraw/Utilitiy/Constants.kt
@@ -2,3 +2,4 @@ package com.example.bjoerndell.navdraw.Utilitiy
 
 const val EXTRA_ATHLETE = "Athlete"
 const val EXTRA_MARTIALARTTYPE = "MartialArt"
+const val EXTRA_PASSWORDLENGTH = 6
